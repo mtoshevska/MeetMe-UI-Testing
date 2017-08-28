e05aca0b98d3788a9ddb0d38193c82b3b475cb8a
Martina <martina.tosevska.95@gmail.com>
2017-08-28 14:08:36 +0200

    Initial commit

---
+++ src/test/java/Example.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class Example {
+
+	public static WebDriver driver;
+	public static String baseUrl;
+
+	@Before
+	public void setUp() throws Exception {
+		File pathToBinary = new File("D:\\firefox-sdk\\bin\\firefox.exe");
+		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+		FirefoxProfile firefoxProfile = new FirefoxProfile();
+		driver = new FirefoxDriver(ffBinary, firefoxProfile);
+		baseUrl = "localhost:8000";
+	}
+	
+	@Test
+	public void test() {
+		driver.get(baseUrl + "/");
+	}
+
+}
