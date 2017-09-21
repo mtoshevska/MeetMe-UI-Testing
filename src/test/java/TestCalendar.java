8b2e24c077bfcbb0e8260a7f685270b1f8fb9e32
Martina <martina.tosevska.95@gmail.com>
2017-09-22 00:33:07 +0200

    Add basic test configurations for pages: Calendar, Profile, Notifications and Requests

---
+++ src/test/java/TestCalendar.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class TestCalendar {
+
+	public static WebDriver driver;
+	public static String baseUrl;
+
+	@Before
+	public void setUp() throws Exception {
+		File pathToBinary = new File("D:\\firefox-sdk\\bin\\firefox.exe");
+		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+		FirefoxProfile firefoxProfile = new FirefoxProfile();
+		driver = new FirefoxDriver(ffBinary, firefoxProfile);
+		baseUrl = "localhost:8000";
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
